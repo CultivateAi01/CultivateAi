c8f59ccb1f9b9ed75ff79e0b54e96bb7d34718ea
CultivateAi01 <cultivateai01@gmail.com>
2025-07-01 08:55:58 +0530

    Updated Hero.tsx

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -90,7 +90,7 @@ export const Hero: React.FC = () => {
           <span className="text-white block">
             into{' '}
             <span className="bg-gradient-to-r from-green-400 to-emerald-500 bg-clip-text text-transparent">
-              startups
+              startup
             </span>
             , in minutes
           </span>
